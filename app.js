9bd22ed2faae68bbae02f9ba58bd58185499c40e
Mark Meadows <markk360@live.com>
2021-02-04 19:20:22 +0000

    Starting on some icing styles

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
 var app = new Vue({
   el: '#app',
   data: {
+    recipient: 'Person1',
     message: 'Your message here',
-	cake: 1,
-	font: 1,
-	cakes: [
-		{id : 1, bgColour: '#d8c7cf'},
-		{id : 2, bgColour: '#d8c7cf'},
-		{id : 3, bgColour: '#d8c7cf'},
-	],
-	bgCol2 : "blue"
+    sender: 'Person2',
+    cake: 5,
+    font: 1,
+    icingColour: '#000000',
+    cakes: [
+      { id: 1, bgColour: '#d8c7cf' },
+      { id: 2, bgColour: '#d8c7cf' },
+      { id: 3, bgColour: '#d8c7cf' },
+      { id: 4, bgColour: '#d8c7cf' },
+      { id: 5, bgColour: '#d8c7cf' },
+    ],
+    bgCol2: 'blue',
   },
-  computed : {
-	bodyStyle() {
-		return {
-			background : this.bgColour
-		}
-	},
-	cakeUrl() {
-		return 'img/cake' + this.cake + '.jpg';
-	},
-	imgStyle() {
-		return {
-			boxShadow: '0 0 20px 20px ' + this.bgColour + ' inset',
-			//boxShadow: '0 0 20px 20px red inset',
-			background: 'url(' + this.cakeUrl + ') center center no-repeat'
-		}
-	},
+  computed: {
+    bodyStyle() {
+      return {
+        background: this.bgColour,
+      };
+    },
+    cakeUrl() {
+      return 'img/cake' + this.cake + '.jpg';
+    },
+    imgStyle() {
+      return {
+        boxShadow: '0 0 20px 20px ' + this.bgColour + ' inset',
+        //boxShadow: '0 0 20px 20px red inset',
+        background: 'url(' + this.cakeUrl + ') center center no-repeat',
+      };
+    },
     bgColour() {
-		return this.cakes.find(c => c.id === this.cake).bgColour;
-	}
+      return this.cakes.find((c) => c.id === this.cake).bgColour;
+    },
+    combinedMessage() {
+      let outMessage = '';
+      outMessage += this.recipient.length > 0 ? this.recipient + '\r\n' : '';
+      outMessage += this.message.length > 0 ? this.message + '\r\n' : '';
+      outMessage += this.sender.length > 0 ? this.sender + '\r\n' : '';
+
+      return outMessage;
+    },
   },
-  methods : {
+  methods: {
     doThing() {
       this.message = 'Hai';
-    }
-  }
+    },
+  },
 });
